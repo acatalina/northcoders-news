b46501e6700f1996ef4c45b92bdcd4a5bc322410
acatalina <accarrasco@gmail.com>
2017-04-07 23:27:49 +0100

    add users

--- src/reducer/index.reducer.js
+++ src/reducer/index.reducer.js
@@ -3,9 +3,11 @@ import {combineReducers} from 'redux';
 import articlesReducer from './articles.reducer';
 import commentsReducer from './comments.reducer';
 import topicsReducer from './topics.reducer';
+import usersReducer from './users.reducer';
 
 export default combineReducers({
 	articles: articlesReducer,
 	comments: commentsReducer,
-	topics: topicsReducer
+	topics: topicsReducer,
+	users: usersReducer
 });
---
+++ src/reducer/users.reducer.js
@@ -0,0 +1,43 @@
+import * as types from '../actions/types';
+
+const initialState = {
+  data: {},
+  fetching: false,
+  error: null
+};
+
+function usersReducer (prevState = initialState, action) {
+  switch (action.type) {
+    case types.FETCH_USERS_REQUEST: {
+      const newState = Object.assign({}, prevState);
+      
+      newState.fetching = true;
+      return newState;
+    }
+    case types.FETCH_USERS_SUCCESS: {
+      const newState = Object.assign({}, prevState);
+      
+      newState.data = reduceToObjectbyUsername(action.data);
+      newState.fetching = false;
+      return newState;
+    }
+    case types.FETCH_USERS_ERROR: {
+      const newState = Object.assign({}, prevState);
+      
+      newState.error = action.error;
+      newState.fetching = false;
+      return newState;
+    }
+    default:
+      return prevState;
+  }
+}
+
+const reduceToObjectbyUsername = (users) => {
+  return users.reduce((res, user) => {
+    res[user.username] = user;
+    return res;
+  }, {});
+};
+
+export default usersReducer;
